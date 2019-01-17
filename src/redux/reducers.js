767c5bc63788b2070883a798da06d41fc1be05bc
xiongzilian <xiongzilian@mintechai.com>
2019-01-17 21:28:35 +0800

    ....

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,8 +1,11 @@
 import counter from 'REDUX/reducers/counter'
+import userInfo from 'REDUX/reducers/userInfo'
+
 
 export default function combineReducers(state = {}, action) {
   return {
-    counter: counter(state.counter, action)
+    counter: counter(state.counter, action),
+    userInfo: userInfo(state.userInfo, action)
   }
 }
 
---
+++ src/redux/reducers/userInfo/index.js
@@ -0,0 +1,57 @@
+import * as TYPE from 'REDUX/actions/actionsTypes'
+
+const initState = {
+  isLoading: false,
+  userInfo: {},
+  errorMsg: ''
+}
+
+export default function reducer(state = initState, action) {
+  switch (action.type) {
+    case TYPE.GET_USER_INFO_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        userInfo: {},
+        errorMsg: ''
+      }
+    case TYPE.GET_USER_INFO_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        userInfo: action.userInfo,
+        errorMsg: ''
+      }
+    case TYPE.GET_USER_INFO_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        userInfo: {},
+        errorMsg: '请求错误'
+      }
+    default:
+      return state
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
